4c5772b411606e4b80da6d2b06d17d11254a1c95
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2018-02-14 22:00:07 +0100

    Added level complete-progress bar to HUD

--- GUI.gd
+++ GUI.gd
@@ -2,6 +2,8 @@ extends MarginContainer
 
 export (int) var score = 0
 export (int) var stars_remaining = 0
+export (int) var total_stars = 0
+export (int) var stars_found = 0
 
 
 func _ready():
@@ -12,5 +14,6 @@ func _ready():
 func _process(delta):
 	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
-	$HBoxContainer/VSplitContainer/Score.text = "Score: " + str(score)
-	$HBoxContainer/VSplitContainer/Stars.text = "Stars remaining: " + str(stars_remaining)
+	$HBox/VSplit/Score.text = "Score: " + str(score)
+	$HBox/VSplit/VSplit/ProgressBar.max_value = total_stars
+	$HBox/VSplit/VSplit/ProgressBar.value = stars_found
--- main.gd
+++ main.gd
@@ -1,11 +1,8 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 var score = 0
-
+var stars_found = 0
+var total_stars = 0
 var current_level
 
 func _ready():
@@ -13,18 +10,27 @@ func _ready():
 	var level = levelscene.instance()
 	current_level = level
 	add_child(level)
+	
+	total_stars = current_level.total_stars
+	stars_found = current_level.stars_found
+	
 	$HUD/GUI.score = score
-	$HUD/GUI.stars_remaining = current_level.stars_remaining
-	level.connect("increase_score", self, "on_increase_score")
+	$HUD/GUI.stars_remaining = total_stars - stars_found
+	$HUD/GUI.total_stars = total_stars
+	$HUD/GUI.stars_found = stars_found
+	
+	level.connect("star_was_taken", self, "on_star_was_taken")
 
-	# Called every time the node is added to the scene.
-	# Initialization here
 	pass
 
-func on_increase_score():
+func on_star_was_taken():
 	score = score + 10
+	total_stars = current_level.total_stars
+	stars_found = current_level.stars_found
 	$HUD/GUI.score = score
-	$HUD/GUI.stars_remaining = current_level.stars_remaining
+	$HUD/GUI.stars_remaining = total_stars - stars_found
+	$HUD/GUI.total_stars = total_stars
+	$HUD/GUI.stars_found = stars_found
 	
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
