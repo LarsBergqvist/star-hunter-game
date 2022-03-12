cd27f182e7ae45f005d5f1b686567078445eeed4
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2022-03-12 19:35:59 +0100

    Transitions for options screen

--- game/OptionsScreen.gd
+++ game/OptionsScreen.gd
@@ -3,6 +3,9 @@ extends Control
 const num_characters = 4
 const num_levels = 5
 
+onready var _transition_rect := $SceneTransitionRect
+
+
 func _ready():
 	for character in range(1, num_characters + 1):
 		var node = self.get_node("Menu/HBoxCharacters/Char" + str(character) + "/Button")
@@ -18,13 +21,15 @@ func _ready():
 	_on_CharButton_pressed(global.character)
 	_on_LevelButton_pressed(global.startLevel)
 
+
 func _on_back_button_pressed():
-	self.queue_free()
-	get_tree().change_scene("res://TitleScreen.tscn")
+	_transition_rect.transition_to("res://TitleScreen.tscn", self)
+
 
 func _clear_all_level_selections():
 	for level in range(1, num_levels + 1):
 		self.get_node("Menu/HBoxLevels/Lev" + str(level)).self_modulate.a8 = 150
+
 
 func _on_LevelButton_pressed(level):
 	_clear_all_level_selections()
