eb17241332eeb0158cbae96b62da37e68db00b18
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2023-07-12 12:08:25 +0200

    Game button adjustment

--- game/action_buttons.gd
+++ game/action_buttons.gd
@@ -2,5 +2,5 @@ extends TextureButton
 
 func _ready():
 	var hasTouchScreen = OS.has_touchscreen_ui_hint()
-	$TextureButton/JumpButton/Label.visible = hasTouchScreen
-	$TextureButton2/ShootButton/Label.visible = hasTouchScreen
+	$TextureButton.visible = hasTouchScreen
+	$TextureButton2.visible = hasTouchScreen
