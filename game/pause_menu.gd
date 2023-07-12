7b6aa7d7bfd7c2cb781e738235fe4b34c4bd0849
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2023-07-12 12:58:37 +0200

    Replaced pause menu item lists with buttons

--- game/pause_menu.gd
+++ game/pause_menu.gd
@@ -8,44 +8,42 @@ signal requested_goto_next_level
 func show_game_paused_menu():
 	$Message.text = "Game paused"
 	$Message.show()
-	$GamePausedMenu.show()
+	$GamePaused.show()
 
 
 func show_game_over_menu():
 	$Message.text = "Game over..."
 	$Message.show()
-	$GameOverMenu.show()
+	$GameOver.show()
 
 func show_level_complete_menu():
 	$Message.text = "Level complete!"
 	$Message.show()
-	$LevelCompleteMenu.show()
+	$LevelComplete.show()
 
 func hide_menu():
 	$Message.hide()
-	$GamePausedMenu.hide()
-	$GameOverMenu.hide()
-	$LevelCompleteMenu.hide()
+	$LevelComplete.hide()
+	$GamePaused.hide()
+	$GameOver.hide()
 
-func _on_GamePausedMenu_item_selected(index):
+func _on_Replay_pressed():
 	hide_menu()
-	if (index == 0):
-		emit_signal("requested_resume_game")
-	elif (index == 1):
-		emit_signal("requested_start_new_game")
+	emit_signal("requested_replay_level")
+	pass # Replace with function body.
 
 
-func _on_GameOverMenu_item_selected(index):
+func _on_NextLevel_pressed():
 	hide_menu()
-	if (index == 0):
-		emit_signal("requested_replay_level")
-	elif (index == 1):
-		emit_signal("requested_start_new_game")
+	emit_signal("requested_goto_next_level")
+	pass # Replace with function body.
 
 
-func _on_LevelCompleteMenu_item_selected(index):
+func _on_Resume_pressed():
 	hide_menu()
-	if (index == 0):
-		emit_signal("requested_goto_next_level")
-	elif (index == 1):
-		emit_signal("requested_replay_level")
+	emit_signal("requested_resume_game")
+
+
+func _on_NewGame_pressed():
+	hide_menu()
+	emit_signal("requested_start_new_game")
