fc84f36df78eb48a6f8490715d3d4843fb2a777f
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2023-07-20 19:06:09 +0200

    test version

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1689226373|1181450998";
+const CACHE_VERSION = "1689872748|3358274678";
 const CACHE_PREFIX = "Star Hunter-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
