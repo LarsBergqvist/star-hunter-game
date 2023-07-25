10e0f7ed4bba148ed8a42e9181f1d4e7ae482bbc
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2023-07-25 12:38:32 +0200

    Instructions screen

--- game/title_screen.gd
+++ game/title_screen.gd
@@ -6,6 +6,7 @@ func _ready()->void:
 	$Menu/NewGame.connect("pressed", self, "_on_newgame_button_pressed")
 	$Menu/Options.connect("pressed", self, "_on_options_button_pressed")
 	$Menu/Leaderboard.connect("pressed", self, "_on_leaderboard_button_pressed")
+	$Menu/Instructions.connect("pressed", self, "_on_instructions_button_pressed")
 
 
 func _on_newgame_button_pressed()->void:
@@ -16,3 +17,8 @@ func _on_options_button_pressed()->void:
 	
 func _on_leaderboard_button_pressed()->void:
 	_transition_rect.transition_to("res://highscores/leaderboard.tscn", self)
+	
+	
+func _on_instructions_button_pressed()->void:
+	_transition_rect.transition_to("res://instructions.tscn", self)
+
