0ff0797132c46e87d69de02b8ee02af539fc9bd9
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2023-07-25 12:51:17 +0200

    Added new variables

--- game/global.gd
+++ game/global.gd
@@ -1,6 +1,10 @@
 extends Node
-
 var character = 2
 var startLevel = 1
 var ammo = 0
-var initial_ammo = 100
+var initial_ammo = 10
+var last_score = 0
+var last_name = ""
+var testing = false
+var apikey = ""
+var gameid = ""
