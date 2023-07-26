e235b407188f762d55b58f32a27e968ed3fe179e
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2023-07-26 08:34:34 +0200

    centered leaderboard data

--- game/highscores/scoretemplate.gd
+++ game/highscores/scoretemplate.gd
@@ -1,11 +1,11 @@
-extends MarginContainer
+extends HBoxContainer
 
 
 func _ready():
 	pass
 	
 func set_Name(name):
-	$Name.text = name
+	$Name.text = name.left(20)
 
 func set_Score(score):
 	$Score.text = score
