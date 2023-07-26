dbf005c9f5a5d5a759ca1c37eb150eee2d0c942d
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2023-07-26 09:15:37 +0200

    Misc clean-up

--- game/highscores/leaderboard.gd
+++ game/highscores/leaderboard.gd
@@ -4,21 +4,24 @@ onready var _transition_rect := $SceneTransitionRect
 
 func _ready():
 	$BackButton.connect("pressed", self, "_on_back_button_pressed")
-	var global = get_node("/root/global")
 	$HTTPRequest.connect("request_completed", self, "_on_request_completed")
 	var headers = ["ApiKey: {apikey}".format({"apikey": global.apikey})]
 	$Status.text = "Loading..."
-	$HTTPRequest.request("https://highscore-service-lb-api.azurewebsites.net/highscore-lists/{gameid}".format({"gameid": global.gameid}), headers)
+	$HTTPRequest.request("{endpoint}/highscore-lists/{gameid}".format({"endpoint" : global.highscore_api, "gameid": global.gameid}), headers)
 
 
 func _on_back_button_pressed():
 	_transition_rect.transition_to("res://title_screen.tscn", self)
 	
 
-func _on_request_completed(result, response_code, headers, body):
+func _on_request_completed(result, _response_code, _headers, body):
+	if result != OK:
+		loading_failed()
+		return
+
 	var json = JSON.parse(body.get_string_from_utf8())
 	if json.error != OK:
-		$Status.text = "Failed loading highscores"
+		loading_failed()
 		return
 
 	$Status.text = ""
@@ -31,5 +34,5 @@ func _on_request_completed(result, response_code, headers, body):
 			$Scroll/Leaderboard.add_child(instance)
 
 
-func _on_HTTPRequest_request_completed(result, response_code, headers, body):
-	pass # Replace with function body.
+func loading_failed():
+	$Status.text = "Failed loading highscores"
--- game/highscores/scoretemplate.gd
+++ game/highscores/scoretemplate.gd
@@ -1,12 +1,9 @@
 extends HBoxContainer
 
 
-func _ready():
-	pass
-	
 func set_Name(name):
 	$Name.text = name.left(20)
 
+
 func set_Score(score):
 	$Score.text = score
-
