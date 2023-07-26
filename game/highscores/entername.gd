9763b0e1ac19cc4ff2a5876776ff15700e1abb4f
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2023-07-26 09:17:11 +0200

    Clean up (#13)
    * test layout
    
    * centered leaderboard data
    
    * Misc clean-up

--- game/highscores/entername.gd
+++ game/highscores/entername.gd
@@ -24,24 +24,36 @@ func _on_Save_pressed():
 		"score": global.last_score
 	}
 	
-	var global = get_node("/root/global")	
+	global.last_name = $VBox/Name.text
 	var query = JSON.print( data_to_send)
 	var headers = ["Content-Type: application/json", "ApiKey: {apikey}".format({"apikey": global.apikey})]
 	var use_ssl = true
-	var url = "https://highscore-service-lb-api.azurewebsites.net/highscore-lists/{gameid}/game-results".format({"gameid": global.gameid})
+	var url = "{endpoint}/highscore-lists/{gameid}/game-results".format({"endpoint" : global.highscore_api, "gameid": global.gameid})
 	$HTTPRequest.connect("request_completed", self, "_on_request_completed")
-	var error = $HTTPRequest.request(url, headers, use_ssl, HTTPClient.METHOD_POST, query)
+	$HTTPRequest.request(url, headers, use_ssl, HTTPClient.METHOD_POST, query)
 	$Status.text = "Saving..."
 
 
 func _on_Cancel_pressed():
 	emit_signal("to_title_screen")
-	
-func _on_request_completed(result, response_code, headers, body):
+
+
+func _on_request_completed(result, _response_code, _headers, body):
+	if result != OK:
+		saving_failed()
+		return
+
 	var json = JSON.parse(body.get_string_from_utf8())
 	if json.error != OK:
-		print("Failure!")
+		saving_failed()
+		return
 
 	$Status.text = ""
 	
+	emit_signal("to_title_screen")
+
+func saving_failed():
+	$Status.text = "Saving failed..."
+	yield(get_tree().create_timer(3.0),"timeout")
+	$Status.text = ""
 	emit_signal("to_title_screen")
